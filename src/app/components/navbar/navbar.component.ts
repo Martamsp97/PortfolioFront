9ee1c1b1cb13dd374eabfc76f0114311a7a9e063
Marta Sánchez <marta.msp97@gmail.com>
2025-05-18 14:10:38 +0200

    Arreglos mediaqueries

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  isOpen = false;
+  menuBurg() {
+    this.isOpen = !this.isOpen;
+  }
 }
